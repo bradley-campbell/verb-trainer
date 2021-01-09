5822a22158da405abf40ffa7726897ac316c819c
frenger88 <64667571+frenger88@users.noreply.github.com>
2021-01-09 16:35:27 -0500

    Initial commit

---
+++ client-FE/public/dataShape.js
@@ -0,0 +1,173 @@
+// Converted to JavaScript object
+
+const manger = {
+  infinitif: {
+    infinitifPrésent: "manger",
+  },
+  indicatif: {
+    présent: {
+      s1: "mange",
+      s2: "manges",
+      s3: "mange",
+      p1: "mangeons",
+      p2: "mangez",
+      p3: "mangent",
+    },
+    imparfait: {
+      s1: "mangeais",
+      s2: "mangeais",
+      s3: "mangeait",
+      p1: "mangions",
+      p2: "mangiez",
+      p3: "mangeaient",                     
+    },
+    futur: {
+      s1: "mangerai",
+      s2: "mangeras",
+      s3: "mangera",
+      p1: "mangerons",
+      p2: "mangerez",
+      p3: "mangeront",
+    },
+    passéSimple: {
+      s1: "mangeai",
+      s2: "mangeas",
+      s3: "mangea",
+      p1: "mangeâmes",
+      p2: "mangeâtes",
+      p3: "mangèrent",
+    },
+  },
+  conditionnel: {
+    présent: {
+      s1: "mangerais",
+      s2: "mangerais",
+      s3: "mangerait",
+      p1: "mangerions",
+      p2: "mangeriez",
+      p3: "mangeraient",
+    },
+  },
+  subjonctif: {
+    présent: {
+      s1: "mange",
+      s2: "manges",
+      s3: "mange",
+      p1: "mangions",
+      p2: "mangiez",
+      p3: "mangent",
+    },
+    imparfait: {
+      s1: "mangeasse",
+      s2: "mangeasses",
+      s3: "mangeât",
+      p1: "mangeassions",
+      p2: "mangeassiez",
+      p3: "mangeassent",
+    },
+  },
+  imperatif: {
+    imperatifPrésent: {
+      s2: "mange",
+      p1: "mangeons",
+      p2: "mangez",
+    },
+  },
+  participe: {
+    participePrésent: "mangeant",
+    participePassé: {
+      ms: "mangé",
+      mp: "mangés",
+      fs: "mangée",
+      fp: "mangées",
+    },
+  },
+};
+
+// mlconjug3 manger - output to command line
+
+// {
+//     "manger": {
+//         "Infinitif": {
+//             "Infinitif Présent": "manger"
+//         },
+//         "Indicatif": {
+//             "Présent": {
+//                 "1s": "mange",
+//                 "2s": "manges",
+//                 "3s": "mange",
+//                 "1p": "mangeons",
+//                 "2p": "mangez",
+//                 "3p": "mangent"
+//             },
+//             "Imparfait": {
+//                 "1s": "mangeais",
+//                 "2s": "mangeais",
+//                 "3s": "mangeait",
+//                 "1p": "mangions",
+//                 "2p": "mangiez",
+//                 "3p": "mangeaient"
+//             },
+//             "Futur": {
+//                 "1s": "mangerai",
+//                 "2s": "mangeras",
+//                 "3s": "mangera",
+//                 "1p": "mangerons",
+//                 "2p": "mangerez",
+//                 "3p": "mangeront"
+//             },
+//             "Passé Simple": {
+//                 "1s": "mangeai",
+//                 "2s": "mangeas",
+//                 "3s": "mangea",
+//                 "1p": "mangeâmes",
+//                 "2p": "mangeâtes",
+//                 "3p": "mangèrent"
+//             }
+//         },
+//         "Conditionnel": {
+//             "Présent": {
+//                 "1s": "mangerais",
+//                 "2s": "mangerais",
+//                 "3s": "mangerait",
+//                 "1p": "mangerions",
+//                 "2p": "mangeriez",
+//                 "3p": "mangeraient"
+//             }
+//         },
+//         "Subjonctif": {
+//             "Présent": {
+//                 "1s": "mange",
+//                 "2s": "manges",
+//                 "3s": "mange",
+//                 "1p": "mangions",
+//                 "2p": "mangiez",
+//                 "3p": "mangent"
+//             },
+//             "Imparfait": {
+//                 "1s": "mangeasse",
+//                 "2s": "mangeasses",
+//                 "3s": "mangeât",
+//                 "1p": "mangeassions",
+//                 "2p": "mangeassiez",
+//                 "3p": "mangeassent"
+//             }
+//         },
+//         "Imperatif": {
+//             "Imperatif Présent": {
+//                 "2s": "mange",
+//                 "1p": "mangeons",
+//                 "2p": "mangez"
+//             }
+//         },
+//         "Participe": {
+//             "Participe Présent": "mangeant",
+//             "Participe Passé": {
+//                 "ms": "mangé",
+//                 "mp": "mangés",
+//                 "fs": "mangée",
+//                 "fp": "mangées"
+//             }
+//         }
+//     }
+// }
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { getConjug } = require("./handlers");
+
+const PORT = 3333;
+
+let app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/conjug", getConjug);
+
+const server = app.listen(PORT, () => {
+  console.info("🌍 Listening on port " + server.address().port);
+});
