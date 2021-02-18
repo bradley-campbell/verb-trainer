353eb95d010bfaa4253cbf83c046ef57600d141b
Bradley <bradley.campbell@osedea.com>
2021-02-18 10:10:17 -0500

    verbQuerySlice added, query on homepage works

--- client/src/components/FilterRadioGroup/MoodRadio.js
+++ client/src/components/FilterRadioGroup/MoodRadio.js
@@ -1,9 +1,22 @@
 import React, { useContext } from "react";
 import { Form, Radio, Segment } from "semantic-ui-react";
 import QueryContext from "../../context/QueryContext";
+import { useDispatch, useSelector } from "react-redux";
+
+import verbQuery, {
+  setVerb,
+  setMood,
+  setOptions,
+} from "../../redux/verbQuerySlice";
 
 const MoodRadio = ({ data, label }) => {
   const { query, setQuery } = useContext(QueryContext);
+
+  const { verb, mood, tense, person, options } = useSelector(
+    (state) => state.reducer
+  );
+
+  const dispatch = useDispatch();
 
   return (
     <Form>
@@ -19,6 +32,7 @@ const MoodRadio = ({ data, label }) => {
                 checked={query[label] === mood}
                 onChange={() => {
                   setQuery({ ...query, [label]: mood });
+                  dispatch(setMood(mood));
                 }}
               />
             </Form.Field>
--- client/src/views/HomePage/index.js
+++ client/src/views/HomePage/index.js
@@ -1,15 +1,19 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import FilterRadioGroup from "../../components/FilterRadioGroup";
 import SearchBar from "../../components/SearchBar";
-import QueryContext from "../../context/QueryContext";
+
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
   const {
     query: { verb, mood, person, tense },
-  } = useContext(QueryContext);
+  } = useSelector((state) => state.reducer);
 
   const handleFetch = async () => {
+    console.log(
+      `/conjug?verb=${verb}&personne=${person}&temps=${tense}&mood=${mood}`
+    );
     const url = `/conjug?verb=${verb}&personne=${person}&temps=${tense}&mood=${mood}`;
     const response = await fetch(url);
     const parsed = await response.json();
